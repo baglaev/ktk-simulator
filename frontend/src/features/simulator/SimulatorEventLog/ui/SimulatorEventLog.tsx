8400d67cf11bed50f6edd0ebe06bc0083d446f97
baglaev <bglvssh@yandex.ru>
2026-08-06 16:20:04 +0300

    feat: event log

--- frontend/src/features/simulator/SimulatorEventLog/ui/SimulatorEventLog.tsx
+++ frontend/src/features/simulator/SimulatorEventLog/ui/SimulatorEventLog.tsx
@@ -1,5 +1,38 @@
+import { Text } from '@consta/uikit/Text';
+import { Table, type TableColumn } from '@consta/table/Table';
+
+type Row = { time: string; description: string };
+
 export const SimulatorEventLog = () => {
-  return <div>SimulatorEventLog</div>;
+  const columns: TableColumn<Row>[] = [
+    {
+      title: 'Время события',
+      accessor: 'time',
+      width: 100,
+    },
+    {
+      title: 'Описание события',
+      accessor: 'description',
+    },
+  ];
+
+  const rows: Row[] = [
+    {
+      time: '00:01',
+      description: 'Сценарий запущен',
+    },
+    {
+      time: '00:04',
+      description: 'Выбран обучающий режим',
+    },
+  ];
+
+  return (
+    <section>
+      <Text>Журнал событий</Text>
+      <Table columns={columns} rows={rows} />
+    </section>
+  );
 };
 
 SimulatorEventLog.displayName = 'SimulatorEventLog';
