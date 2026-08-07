5f8233f1041bbf1c39621e90d3111ea668d203a1
baglaev <bglvssh@yandex.ru>
2026-08-07 15:08:38 +0300

    feat: scheme integrate

--- frontend/src/features/simulator/SimulatorSchema/ui/SimulatorSchema.tsx
+++ frontend/src/features/simulator/SimulatorSchema/ui/SimulatorSchema.tsx
@@ -1,5 +1,41 @@
+import SchemeSvg from '@/shared/assets/scheme.svg?react';
+
 export const SimulatorSchema = () => {
-  return <div>SimulatorSchema</div>;
+  const clickableIds = [
+    'pump-h1',
+    'pump-h1a',
+    'pump-h1b',
+    'pump-h1v',
+    'heat-exchanger-t1-t11',
+    'elou-block',
+    'e15',
+  ];
+
+  const handleSchemeClick = (event: React.MouseEvent<SVGSVGElement>) => {
+    const target = event.target as SVGElement;
+
+    const element = target.closest('g[id]');
+
+    if (!element) {
+      return;
+    }
+
+    let id = element.id;
+
+    if (id.startsWith('pump-shape')) {
+      id = element.parentElement?.parentElement?.id ?? id;
+    }
+
+    if (clickableIds.includes(id)) {
+      console.log('Нажат объект:', id);
+    }
+  };
+
+  return (
+    <div>
+      <SchemeSvg onClick={handleSchemeClick} />
+    </div>
+  );
 };
 
 SimulatorSchema.displayName = 'SimulatorSchema';
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    svgr(),
 
     checker({
       typescript: true,
