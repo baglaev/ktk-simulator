bf6a437a8e5062b2a0fcdaa58d67eedc2cc6eb97
baglaev <bglvssh@yandex.ru>
2026-08-08 11:05:44 +0300

    feat: logo

--- frontend/src/widgets/Header/ui/Header.tsx
+++ frontend/src/widgets/Header/ui/Header.tsx
@@ -1,5 +1,6 @@
 import { User } from '@consta/uikit/User';
 import { Text } from '@consta/uikit/Text';
+import { Picture } from '@consta/uikit/Picture';
 
 import styles from './Header.module.css';
 
@@ -7,10 +8,13 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.titleContainer}>
-        <Text size="l">Компьютерный тренажёрный комплекс</Text>
-        <Text size="l" view="secondary">
-          ЭЛОУ-АВТ
-        </Text>
+        <Picture src="../../../../public/logo.png" className={styles.logo} />
+        <div>
+          <Text size="l">Компьютерный тренажёрный комплекс</Text>
+          <Text size="l" view="secondary">
+            ЭЛОУ-АВТ
+          </Text>
+        </div>
       </div>
       <User name="Демо-профиль" info="Обучаемый" size="l" />
     </header>
